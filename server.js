3470c890cfa6b1d3364fe866fe644661063ec0de
Tiffany White <tiffanywhitedev412@gmail.com>
2017-04-20 22:03:38 -0400

    Updates todo taskpaper file, adds yarn, and Procfile and server.js for heroku

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+var port = process.env.PORT || 8080;
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(port, function () {
+  console.log('Example app listening on port 3000!');
+});
