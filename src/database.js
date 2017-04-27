7fb712aafa8425cc45fb2e9cf8b08bc6312b2f63
Tiffany White <tiffanywhitedev412@gmail.com>
2017-04-26 22:29:13 -0400

    Updates files, adds database js file, and folder hierarchy

---
+++ src/database.js
@@ -0,0 +1,33 @@
+var sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'mysql'|'postgres',
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+});
+
+
+var User = sequelize.define('user', {
+  firstName: {
+    type: Sequelize.STRING,
+    field: 'first_name' // Will result in an attribute that is firstName when user facing but first_name in the database
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    field: 'last_name'
+  }
+},
+{
+  freezeTableName: true // Model tableName will be the same as the model name
+});
+
+User.sync({force: true}).then(function () {
+  // Table created
+  return User.create({
+    firstName: 'John',
+    lastName: 'Hancock'
+  });
+});
