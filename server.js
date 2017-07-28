c47a4fd39bab999dc919aff6764c49e6b2d94574
Tiffany White <tiffanywhitedev412@gmail.com>
2017-07-28 18:12:01 -0400

    Removed postgres from package file, added quick fix to server.js using Herokus port env

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var app = express();
 
 app.use(express.static('public'));
 
-var port = process.env.PORT || 8080;
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
