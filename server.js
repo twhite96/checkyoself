3765c7b910680ca562e012d0937b0609cc51fd7f
Tiffany White <tiffanywhitedev412@gmail.com>
2017-07-28 18:20:50 -0400

    Still working on Heroku errors

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+var port = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.listen(port, function () {
+  console.log('Example app listening on port 3000!');
 });
