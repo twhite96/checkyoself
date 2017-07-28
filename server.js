7c9d0cf25e12061da2517d40b49154acd2795ccd
Tiffany White <tiffanywhitedev412@gmail.com>
2017-07-28 18:12:48 -0400

    Added duplicate code which I removed in server.js

--- server.js
+++ server.js
@@ -10,7 +10,3 @@ app.listen(process.env.PORT || 3000, function(){
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-
-app.listen(port, function () {
-  console.log('Example app listening on port 3000!');
-});
