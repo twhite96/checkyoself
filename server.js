6b4e71ccea82f22f4085457c33b7a66e0fcfe5e9
Tiffany White <tiffanywhitedev412@gmail.com>
2017-07-29 04:43:11 -0400

    OMG I FIXED THIS SHIT

--- server.js
+++ server.js
@@ -1,26 +1,66 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-const VIEWS = path.join(__dirname, 'views');
-var port = process.env.PORT || 8080;
 
-app.set('view engine', 'html');
-app.use(express.static('public'));
+app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, '/public')));
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-  res.sendFile('index.html', { root : VIEWS });
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+app.get('/texts.ejs', function(request, response) {
+  response.render('pages/texts');
 });
 
-app.get("/faq.html", function (req, res) {
-  res.sendFile('faq.html', { root : VIEWS });
+app.get('/faq.ejs', function(request, response) {
+  response.render('pages/faq');
+});
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
 
-app.get("/texts.html", function (req, res) {
-  res.sendFile('texts.html', { root : VIEWS });
-});
 
-app.listen(port, function () {
-  console.log('Example app listening on 8080!');
-});
+
+
+
+// let express = require("express");
+// let app = express();
+// let path = require("path");
+// let port = process.env.PORT || 8080;
+//
+// app.use(express.static(__dirname + '/public'));
+//
+// // views is directory for all template files
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
+//
+// app.get('/', function(req, res) {
+//   res.render('pages/index')
+// });
+
+
+// app.set("view engine", "ejs");
+// app.engine('html', require('ejs').renderFile);
+//
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.set('views', path.join(__dirname, 'views'));
+//
+// app.get('/', function (req, res) {
+//   res.render('index');
+// });
+
+// app.get('/', function (req, res) {
+//   res.render('faq');
+// });
+//
+// app.get('/', function (req, res) {
+//   res.render('texts');
+// });
+
+// app.listen(port, function () {
+//   console.log('Example app listening on 8080!');
+// });
