78c92ccd673c78b7447c8a4b372b10ba6c85f5ae
Tiffany White <tiffanywhitedev412@gmail.com>
2017-07-28 20:49:13 -0400

    Working for now

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 var express = require('express');
 var app = express();
-
-app.use(express.static('/'));
-
+var path = require('path');
+const VIEWS = path.join(__dirname, 'views');
 var port = process.env.PORT || 8080;
+
+app.set('view engine', 'html');
+app.use(express.static('public'));
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile('index.html', { root : VIEWS });
+});
+
+app.get("/faq.html", function (req, res) {
+  res.sendFile('faq.html', { root : VIEWS });
+});
+
+app.get("/texts.html", function (req, res) {
+  res.sendFile('texts.html', { root : VIEWS });
 });
 
 app.listen(port, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on 8080!');
 });
