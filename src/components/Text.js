b1360bed6af21c5eadc1fb103337182f92f6fd77
Tiffany White <hey@tiffanyrwhite.com>
2019-01-25 17:14:37 -0500

    Comment Text.js logic out until bugs are fixed. Will break site

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,47 +1,47 @@
-import React from 'react';
-import Popup from 'reactjs-popup';
-import BurgerIcon from '../components/BurgerIcon';
-import Menu from '../components/Menu';
-import { Editor, EditorState, RichUtils } from 'md-draft-js';
-import {WriteGood, suggestionStrategy} from '../helpers/write_good';
-import Footer from '../components/Footer';
+// import React from 'react';
+// import Popup from 'reactjs-popup';
+// import BurgerIcon from '../components/BurgerIcon';
+// import Menu from '../components/Menu';
+// import { Editor, EditorState, RichUtils } from 'md-draft-js';
+// import {WriteGood, suggestionStrategy} from '../helpers/write_good';
+// import Footer from '../components/Footer';
 
-class Text extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { editorState: EditorState.createWithContent('Write your markdown here.') };
-    this.onChange = (editorState) => this.setState({ editorState });
-    this.handleKeyCommand = this.handleKeyCommand.bind(this);
-  }
-  render() {
-    return (
-      <React.Fragment>
-        <div>
-          <div style={styles}>
-            <Popup
-              modal
-              overlayStyle={{ background: 'rgba(255,255,255,0.98)' }}
-              contentStyle={contentStyle}
-              closeOnDocumentClick={false}
-              trigger={open => <BurgerIcon open={open} />}
-            >
-              {close => <Menu close={close} />}
-            </Popup>
-            <Editor
-              autoFocus
-              className="editor-textarea"
-              editorState={this.state.editorState}
-              onKeyCommand={this.handleKeyCommand}
-              onChange={this.onChange}
-            />
-          </div>
-        </div>
-        <div>
-          <Footer />
-        </div>
-      </React.Fragment>
-    );
-  }
-}
+// class Text extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { editorState: EditorState.createWithContent('Write your markdown here.') };
+//     this.onChange = (editorState) => this.setState({ editorState });
+//     this.handleKeyCommand = this.handleKeyCommand.bind(this);
+//   }
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <div>
+//           <div style={styles}>
+//             <Popup
+//               modal
+//               overlayStyle={{ background: 'rgba(255,255,255,0.98)' }}
+//               contentStyle={contentStyle}
+//               closeOnDocumentClick={false}
+//               trigger={open => <BurgerIcon open={open} />}
+//             >
+//               {close => <Menu close={close} />}
+//             </Popup>
+//             <Editor
+//               autoFocus
+//               className="editor-textarea"
+//               editorState={this.state.editorState}
+//               onKeyCommand={this.handleKeyCommand}
+//               onChange={this.onChange}
+//             />
+//           </div>
+//         </div>
+//         <div>
+//           <Footer />
+//         </div>
+//       </React.Fragment>
+//     );
+//   }
+// }
 
 export default Text;
