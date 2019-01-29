a2585bb07701d3bbce040d2ed7ebc44bf46a3a1f
Tiffany White <hey@tiffanyrwhite.com>
2019-01-29 13:55:59 -0500

    Added old Text component back until new one is completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,19 +4,12 @@ import { Hero, SubTitle, Container } from 'reactbulma';
 import '../smde-editor.css';
 
 class Footer extends React.Component {
-
-  // componentDidMount() {
-  //   const s = document.createElement('script');
-  //   s.type = 'text/javascript';
-  //   s.innerHTML = document.write(new Date().getFullYear());
-  //   this.instance.appendChild(s);
-  // }
   render() {
     return (
       <Hero className="change-header push-down">
         <Hero.Foot>
           <Container>
-            <SubTitle className="change-text">2018 &copy; Tiffany White</SubTitle>
+            <SubTitle className="change-text"> &copy; 2017 - {(new Date().getFullYear())} Tiffany White</SubTitle>
             <SubTitle className="change-text">
               Made with ❤️in Pittsburgh
             </SubTitle>
--- src/components/Text.js
+++ src/components/Text.js
@@ -1,47 +1,107 @@
-// import React from 'react';
-// import Popup from 'reactjs-popup';
-// import BurgerIcon from '../components/BurgerIcon';
-// import Menu from '../components/Menu';
-// import { Editor, EditorState, RichUtils } from 'md-draft-js';
-// import {WriteGood, suggestionStrategy} from '../helpers/write_good';
-// import Footer from '../components/Footer';
+/* jshint ignore: start */
+import React from 'react';
+import SimpleMDEReact from 'react-simplemde-editor';
+import 'simplemde/dist/simplemde.min.css';
+import Popup from 'reactjs-popup';
+import BurgerIcon from '../components/BurgerIcon';
+import Menu from '../components/Menu';
+import '../smde-editor.css';
+import writeGood from 'write-good';
+import Footer from '../components/Footer';
 
-// class Text extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { editorState: EditorState.createWithContent('Write your markdown here.') };
-//     this.onChange = (editorState) => this.setState({ editorState });
-//     this.handleKeyCommand = this.handleKeyCommand.bind(this);
-//   }
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <div>
-//           <div style={styles}>
-//             <Popup
-//               modal
-//               overlayStyle={{ background: 'rgba(255,255,255,0.98)' }}
-//               contentStyle={contentStyle}
-//               closeOnDocumentClick={false}
-//               trigger={open => <BurgerIcon open={open} />}
-//             >
-//               {close => <Menu close={close} />}
-//             </Popup>
-//             <Editor
-//               autoFocus
-//               className="editor-textarea"
-//               editorState={this.state.editorState}
-//               onKeyCommand={this.handleKeyCommand}
-//               onChange={this.onChange}
-//             />
-//           </div>
-//         </div>
-//         <div>
-//           <Footer />
-//         </div>
-//       </React.Fragment>
-//     );
-//   }
-// }
+const styles = {
+  fontFamily: 'sans-serif',
+  textAlign: 'center',
+  marginTop: '40px'
+};
+const contentStyle = {
+  background: 'rgba(255,255,255,0)',
+  width: '80%',
+  border: 'none'
+};
+const editorStyle = {
+  margin: '2em 2em'
+};
+
+const WriteGood = ({ text }) => (
+  <div>{writeGood(text).map(({ suggestion }) => suggestion)}</div>
+);
+
+class Text extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      textValue: 'Check your markdown here.',
+      text: {}
+    };
+  }
+  extraKeys = () => {
+    return {
+      Up: function(cm) {
+        cm.replaceSelection(' surprise. ');
+      },
+      Down: function(cm) {
+        cm.replaceSelection(' surprise again! ');
+      }
+    };
+  };
+
+  handleChange1 = value => {
+    this.setState({
+      textValue: value
+    });
+  };
+
+  handleTextChange = markup => {
+    this.setState({
+      text: markup
+    });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>
+          <div style={styles}>
+            <Popup
+              modal
+              overlayStyle={{ background: 'rgba(255,255,255,0.98)' }}
+              contentStyle={contentStyle}
+              closeOnDocumentClick={false}
+              trigger={open => <BurgerIcon open={open} />}
+            >
+              {close => <Menu close={close} />}
+            </Popup>
+
+            {/* <button
+            style={{ display: "inline-block", margin: "10px 0" }}
+            onClick={this.handleTextChange}
+          >
+            Click me to update the textValue outside of the editor
+          </button> */}
+            <SimpleMDEReact
+              className="smde-editor-styles"
+              editorStyle={editorStyle}
+              // suggested={this.editorState}
+              label="Markdown Editor"
+              onChange={this.handleChange1}
+              options={{
+                autofocus: true,
+                spellChecker: true
+                // etc.
+              }}
+              value={this.state.textValue}
+              markup={this.state.text}
+            />
+            <WriteGood />
+          </div>
+        </div>
+        <div>
+          <Footer />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
 
 export default Text;
