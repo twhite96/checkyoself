a0a25f3b4fbed5bf783475aea415bbd3708748d8
Tiffany <tiffany@tiffanywhite.dev>
2023-04-23 21:26:28 -0400

    Moving from CRA and old stack

--- src/components/BurgerIcon.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default ({ open, ...props }) => (
-  <div className={open ? "burger-menu open" : "burger-menu"} {...props}>
-    <div className="bar1" key="b1" />
-    <div className="bar2" key="b2" />
-    <div className="bar3" key="b3" />
-  </div>
-);
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+// https://vitejs.dev/config https://vitest.dev/config
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: '.vitest/setup',
+    include: ['**/test.{ts,tsx}']
+  }
+})
